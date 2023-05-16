e4d79b52632acb9d0841e669a7c39989e0442c57
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-05-16 10:21:49 +0200

    Lesson notes

---
+++ Lesson 10/argparse_calc.py
@@ -0,0 +1,32 @@
+import argparse
+
+def addition(x,y):
+    return x + y
+
+def subtraction(x,y):
+    return x - y
+
+
+# if __name__ == '__main__' indicates the entry point to the program when run directly by the Python interpreter.
+# The code inside the if statement is not executed when the file's code is imported as a module.
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("x", type=int)
+    parser.add_argument("y", type=int)
+    
+    # Optional argument with (bool with default False if not provided)
+    parser.add_argument("-s","--subtract", action="store_true")
+    
+    args = parser.parse_args()
+    
+    # Manual typecasting, if type not specified when adding args to parser
+    # x, y = int(args.x), int(args.y)
+
+    if args.subtract:
+        print(subtraction(args.x, args.y)) 
+    else:
+        print(addition(args.x, args.y))
+    
+    quit()
+    
---
+++ Lesson 10/argparse_demo.py
@@ -0,0 +1,23 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# Add expected arguments
+# Positional
+parser.add_argument("echo", help="The string you want printed (echoed)") # prints help when used with -h (--help)
+parser.add_argument("numb", type=int) # Set type explicitly, will fail if not correct type provided via CLA
+# Optional
+parser.add_argument("-opr", "--optional_required") # Optional argument, fails if passed in without trailing arg, default to None if not passed in
+parser.add_argument("-opd", "--optional_default", action="store_true") # Optional argument, defaults to False if not provided
+
+# Get arguments passed in as CLAs
+args = parser.parse_args()
+
+# Handle unknown args
+# args, unknown = parser.parse_known_args()
+
+# Reference arguments using names provided when added
+print(f"""echo:{args.echo}
+numb:{args.numb}
+--optional-required:{args.optional_required}
+--optional_default:{args.optional_default}""")
