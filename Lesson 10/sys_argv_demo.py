f5927c8d40cc9209a5172dd12da2ae99065a39d2
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-05-16 11:25:09 +0200

    Lesson notes

--- Lesson 10/sys_argv_demo.py
+++ Lesson 10/sys_argv_demo.py
@@ -1,6 +1,9 @@
 import sys
 
 print(f"## sys.argv demo")
-print(f"sys.argv is of type {type(sys.argv)}")
-print(f"Number of arguments: {len(sys.argv)}")
-print(f"Arguments: {str(sys.argv)}")
+
+## Access arguments passed to script using 'sys.argv'
+print(f"sys.argv is of type {type(sys.argv)}") # A list
+print(f"Number of arguments: {len(sys.argv)}") # Includes script name as first argument
+print(f"Arguments: {sys.argv}") # Display all arguments
+print(f"Arguments (-first one): {sys.argv[1:]}") # Exclude first argument
