98c4d0694aa9c1a724cad6b7a766500a39d852d2
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2023-06-12 16:02:38 +0200

    Lesson content

--- Lesson 17/TimedLoop.py
+++ Lesson 17/TimedLoop.py
@@ -3,7 +3,7 @@ import time
 max_counter = 10
 counter = 0 
 
-while(counter <= max_counter): 
+while counter <= max_counter: 
     print("Hello World #", counter) 
     counter += 1
     time.sleep(3)
